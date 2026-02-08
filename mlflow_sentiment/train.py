dc21eb0f9601ce5787bfd9d246e54a5ded07b9ea
Mohammad Asadullah <87116101+Asadullah2412@users.noreply.github.com>
2026-02-08 19:44:23 +0530

    code101

---
+++ mlflow_sentiment/train.py
@@ -0,0 +1,70 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+import joblib
+
+
+data = pd.read_csv('data/clean_data')
+
+X = data['clean_review']     # text data
+y = data['sentiment']   
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, 
+    test_size=0.2, 
+    random_state=42,
+    stratify=y
+)
+
+tfidf = TfidfVectorizer(
+    max_features=5000,
+    ngram_range=(1, 2)
+)
+X_train_tfidf = tfidf.fit_transform(X_train)
+X_test_tfidf = tfidf.transform(X_test)
+
+
+
+    # set experiment
+mlflow.set_experiment("Sentiment_Analysis_Flipkart") 
+
+with mlflow.start_run(run_name="NB_baseline"):
+
+    # log parameters
+    mlflow.log_param("model", "MultinomialNB")
+    mlflow.log_param("vectorizer", "TF-IDF")
+    mlflow.log_param("ngram_range", "(1,2)")
+    mlflow.log_param("max_features", 5000)
+    mlflow.log_param("alpha", 1.0)
+    mlflow.log_param("train_size", X_train.shape[0])
+    mlflow.log_param("test_size", X_test.shape[0])
+
+
+    joblib.dump(tfidf, "tfidf_vectorizer.pkl")
+    mlflow.log_artifact("tfidf_vectorizer.pkl")
+
+    # train model (your existing code)
+    nb_model = MultinomialNB(alpha=1.0)
+    nb_model.fit(X_train_tfidf, y_train)
+
+    # predictions
+    y_pred = nb_model.predict(X_test_tfidf)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred, average="weighted")
+    precision = precision_score(y_test, y_pred, average="weighted")
+    recall = recall_score(y_test, y_pred, average="weighted")
+    # metrics
+    mlflow.log_metric("accuracy", accuracy_score(y_test, y_pred))
+    mlflow.log_metric("f1_score", f1_score(y_test, y_pred, average="weighted"))
+    mlflow.log_metric("precision", precision_score(y_test, y_pred, average="weighted"))
+    mlflow.log_metric("recall", recall_score(y_test, y_pred, average="weighted"))
+
+
+    # log model
+    mlflow.sklearn.log_model(nb_model, "sentiment_model")
